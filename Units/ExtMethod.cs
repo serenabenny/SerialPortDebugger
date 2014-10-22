100e9f2604fc425f67af619840410e44527fd773
netpaymet@gmail.com <netpaymet@gmail.com>
2014-10-22 15:13:14 +0800

    更改部分

--- Units/ExtMethod.cs
+++ Units/ExtMethod.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LeafSoft.Units
 {
